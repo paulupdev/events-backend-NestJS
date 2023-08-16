47441d88d8eeee9a70c6278fd44a711e1a027e69
Paul <paulupdev@gmail.com>
2023-08-16 17:36:46 -0400

    JWT Token along with secret handoff is working

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { LocalStrategy } from './local.strategy';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [LocalStrategy],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.AUTH_SECRET,
+        signOptions: { expiresIn: '60m' },
+      }),
+    }),
+  ],
+  providers: [LocalStrategy, AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
