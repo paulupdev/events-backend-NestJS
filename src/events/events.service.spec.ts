1e71df72c6e929eb85ab4c2e4999d228b8b46ca3
Paul <paulupdev@gmail.com>
2023-08-31 18:32:08 -0400

    Event Service Unit test created

---
+++ src/events/events.service.spec.ts
@@ -0,0 +1,30 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EventsService } from './events.service';
+import { Repository } from 'typeorm';
+import { Test } from '@nestjs/testing';
+
+describe('EventsService', () => {
+  let service: EventsService;
+  let repository: Repository<Event>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        EventsService,
+        {
+          provide: getRepositoryToken(Event),
+          useValue: {
+            save: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            delete: jest.fn(),
+            where: jest.fn(),
+            execute: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<EventsService>(EventsService);
+    repository = module.get<Repository<Event>>(getRepositoryToken(Event));
+  });
+});
