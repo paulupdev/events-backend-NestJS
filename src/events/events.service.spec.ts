c90f0a9902ff8596c85b620a69cb55fc04af7e13
Paul <paulupdev@gmail.com>
2023-09-01 17:24:35 -0400

    fixed some issues with services test

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -1,22 +1,28 @@
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { EventsService } from './events.service';
-import { DeleteQueryBuilder, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Test } from '@nestjs/testing';
+import * as paginator from './../pagination/paginator';
+
+jest.mock('./../pagination/paginator');
 
 describe('EventsService', () => {
   let service: EventsService;
   let repository: Repository<Event>;
   let selectQb;
   let deleteQb;
+  let mockedPaginate;
 
   beforeEach(async () => {
+    // mockedPaginate = paginator.paginate as jest.Mock;
+    mockedPaginate = jest.spyOn(paginator, 'paginate');
     deleteQb = {
       where: jest.fn(),
       execute: jest.fn(),
     };
 
     selectQb = {
-      delete: jest.fn().mockReturnValue(DeleteQueryBuilder),
+      delete: jest.fn().mockReturnValue(deleteQb),
       where: jest.fn(),
       execute: jest.fn(),
       orderBy: jest.fn(),
@@ -44,7 +50,7 @@ describe('EventsService', () => {
   });
 
   describe('updateEvent', () => {
-    it('should update an event', async () => {
+    it('should update the event', async () => {
       const repoSpy = jest
         .spyOn(repository, 'save')
         .mockResolvedValue({ id: 1 } as Event);
@@ -53,7 +59,7 @@ describe('EventsService', () => {
           name: 'New name',
         }),
       ).resolves.toEqual({ id: 1 });
-      expect(repoSpy).toBeCalledTimes({ id: 1, name: 'New name' });
+      expect(repoSpy).toBeCalledWith({ id: 1, name: 'New name' });
     });
   });
 
@@ -65,7 +71,7 @@ describe('EventsService', () => {
       );
       const deleteSpy = jest
         .spyOn(selectQb, 'delete')
-        .mockReturnValue(DeleteQueryBuilder);
+        .mockReturnValue(deleteQb);
       const whereSpy = jest.spyOn(deleteQb, 'where').mockReturnValue(deleteQb);
       const executeSpy = jest.spyOn(deleteQb, 'execute');
 
@@ -78,6 +84,54 @@ describe('EventsService', () => {
       expect(whereSpy).toHaveBeenCalledTimes(1);
       expect(whereSpy).toHaveBeenCalledWith('id = :id', { id: 1 });
       expect(executeSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getEventsAttendedByUserIdPaginated', () => {
+    it('should return a list of paginated events', async () => {
+      const orderBySpy = jest
+        .spyOn(selectQb, 'orderBy')
+        .mockReturnValue(selectQb);
+      const leftJoinSpy = jest
+        .spyOn(selectQb, 'leftJoinAndSelect')
+        .mockReturnValue(selectQb);
+      const whereSpy = jest.spyOn(selectQb, 'where').mockReturnValue(selectQb);
+
+      mockedPaginate.mockResolvedValue({
+        first: 1,
+        last: 1,
+        total: 10,
+        limit: 10,
+        data: [],
+      });
+
+      expect(
+        service.getEventsAttendedByUserIdPaginated(500, {
+          limit: 1,
+          currentPage: 1,
+        }),
+      ).resolves.toEqual({
+        data: [],
+        first: 1,
+        last: 1,
+        limit: 10,
+        total: 10,
+      });
+
+      expect(orderBySpy).toBeCalledTimes(1);
+      expect(orderBySpy).toBeCalledWith('e.id', 'DESC');
+
+      expect(leftJoinSpy).toBeCalledTimes(1);
+      expect(leftJoinSpy).toBeCalledWith('e.attendees', 'a');
+
+      expect(whereSpy).toBeCalledTimes(1);
+      expect(whereSpy).toBeCalledWith('a.userId = :userId', { userId: 500 });
+
+      expect(mockedPaginate).toBeCalledTimes(1);
+      expect(mockedPaginate).toBeCalledWith(selectQb, expect.any(Function), {
+        currentPage: 1,
+        limit: 1,
+      });
     });
   });
 });
