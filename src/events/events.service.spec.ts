2ecc8d5ca2734186a72ccd2fe08f68d9467c988c
Paul <paulupdev@gmail.com>
2023-09-01 16:53:09 -0400

    added events services test

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -1,13 +1,28 @@
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { EventsService } from './events.service';
-import { Repository } from 'typeorm';
+import { DeleteQueryBuilder, Repository } from 'typeorm';
 import { Test } from '@nestjs/testing';
 
 describe('EventsService', () => {
   let service: EventsService;
   let repository: Repository<Event>;
+  let selectQb;
+  let deleteQb;
 
   beforeEach(async () => {
+    deleteQb = {
+      where: jest.fn(),
+      execute: jest.fn(),
+    };
+
+    selectQb = {
+      delete: jest.fn().mockReturnValue(DeleteQueryBuilder),
+      where: jest.fn(),
+      execute: jest.fn(),
+      orderBy: jest.fn(),
+      leftJoinAndSelect: jest.fn(),
+    };
+
     const module = await Test.createTestingModule({
       providers: [
         EventsService,
@@ -15,7 +30,7 @@ describe('EventsService', () => {
           provide: getRepositoryToken(Event),
           useValue: {
             save: jest.fn(),
-            createQueryBuilder: jest.fn(),
+            createQueryBuilder: jest.fn().mockReturnValue(selectQb),
             delete: jest.fn(),
             where: jest.fn(),
             execute: jest.fn(),
@@ -26,5 +41,43 @@ describe('EventsService', () => {
 
     service = module.get<EventsService>(EventsService);
     repository = module.get<Repository<Event>>(getRepositoryToken(Event));
+  });
+
+  describe('updateEvent', () => {
+    it('should update an event', async () => {
+      const repoSpy = jest
+        .spyOn(repository, 'save')
+        .mockResolvedValue({ id: 1 } as Event);
+      expect(
+        service.updateEvent(new Event({ id: 1 }), {
+          name: 'New name',
+        }),
+      ).resolves.toEqual({ id: 1 });
+      expect(repoSpy).toBeCalledTimes({ id: 1, name: 'New name' });
+    });
+  });
+
+  describe('deleteEvent', () => {
+    it('should delete an event', async () => {
+      const createQueryBuilderSpy = jest.spyOn(
+        repository,
+        'createQueryBuilder',
+      );
+      const deleteSpy = jest
+        .spyOn(selectQb, 'delete')
+        .mockReturnValue(DeleteQueryBuilder);
+      const whereSpy = jest.spyOn(deleteQb, 'where').mockReturnValue(deleteQb);
+      const executeSpy = jest.spyOn(deleteQb, 'execute');
+
+      expect(service.deleteEvent(1)).resolves.toBe(undefined);
+
+      expect(createQueryBuilderSpy).toHaveBeenCalledTimes(1);
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('e');
+
+      expect(deleteSpy).toHaveBeenCalledTimes(1);
+      expect(whereSpy).toHaveBeenCalledTimes(1);
+      expect(whereSpy).toHaveBeenCalledWith('id = :id', { id: 1 });
+      expect(executeSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
