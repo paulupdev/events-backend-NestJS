ad5caefe92e4df40a460916c2ddc56b64ef5ea24
Paul <paulupdev@gmail.com>
2023-09-12 18:48:14 -0400

    Added delete query to teachers

--- src/school/school.types.ts
+++ src/school/school.types.ts
@@ -1,9 +1,19 @@
-import { registerEnumType } from '@nestjs/graphql';
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
 
 export enum Gender {
   Male = 'Male',
   Female = 'Female',
   Other = 'Other',
+}
+
+@ObjectType()
+export class EntityWithId {
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  @Field(() => Int)
+  id: number;
 }
 
 registerEnumType(Gender, { name: 'Gender' });
--- src/school/teacher.resolver.ts
+++ src/school/teacher.resolver.ts
@@ -13,6 +13,7 @@ import { Repository } from 'typeorm';
 import { TeacherAddInput } from './input/teacher-add.input';
 import { Logger } from '@nestjs/common';
 import { TeacherEditInput } from './input/teacher-edits.input';
+import { EntityWithId } from './school.types';
 
 @Resolver(() => Teacher)
 export class TeacherResolver {
@@ -61,6 +62,21 @@ export class TeacherResolver {
     return await this.teachersRepository.save(
       new Teacher(Object.assign(teacher, input)),
     );
+  }
+
+  @Mutation(() => EntityWithId, { name: 'teacherDelete' })
+  public async delete(
+    @Args('id', { type: () => Int })
+    id: number,
+  ): Promise<EntityWithId> {
+    const teacher = await this.teachersRepository.findOneOrFail({
+      where: {
+        id,
+      },
+    });
+    await this.teachersRepository.remove(teacher);
+
+    return new EntityWithId(id);
   }
 
   @ResolveField('subjects')
